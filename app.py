e1914c7ffde5bcfe997f5ce89262b00ff2e143a6
Tyler Hasenoehrl <tylerhasenoehrl@Tylers-MacBook-Pro.local>
2018-01-27 18:10:04 -0800

    getfiles added and style changes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 # pylint: disable=invalid-name
 # pylint: disable=missing-docstring
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+def getFiles():
+    path = "images"
+    files = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            files.append(file)
+    return files
+
 @app.route("/")
 def index():
-    return render_template('home.html')
+    images = getFiles()
+    return render_template('home.html', images=images)
 
 if __name__ == '__main__':
     app.run()
